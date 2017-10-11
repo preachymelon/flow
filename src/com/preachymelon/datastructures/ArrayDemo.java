aeaa870ccbd4de9f5b857867d7bcbccef2b93e94
Preachy Melon <preachymelon@gmail.com>
2017-10-11 01:27:25 -0700

    Adding array demo

---
+++ src/com/preachymelon/datastructures/ArrayDemo.java
@@ -0,0 +1,11 @@
+package com.preachymelon.datastructures;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
