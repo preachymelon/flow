4f43eed8838e4d9da3650d713ca2ae334a5fe78c
Preachy Melon <preachymelon@gmail.com>
2017-10-11 01:35:59 -0700

    code cleanup

--- src/com/preachymelon/datastructures/ArrayDemo.java
+++ src/com/preachymelon/datastructures/ArrayDemo.java
@@ -3,9 +3,8 @@ package com.preachymelon.datastructures;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World");
-
 	}
 
 }
